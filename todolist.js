f9f50b3e63de0435809228f5f2e831fb78cb06ea
Hua_Nan <x565619@gmail.com>
2021-06-29 15:23:29 +0800

    增加新功能&優化

--- todolist.js
+++ todolist.js
@@ -9,113 +9,140 @@ const btnComplete = document.querySelector('.complete');
 const clear = document.querySelector('.clear');
 const todoList = document.querySelector('.output');
 
-let datas = []
+let datas = localStorage.getItem('datas');
+if (datas) {
+	datas = JSON.parse(datas);//Json to array
+	if (datas.length) {
+		todoList.setAttribute("style", "display:block");
+		renderList();
+	}
+} else {
+	datas = [];
+}
+
 btnAdd.addEventListener("click", function (e) {
-    let obj = {};
-    if (txt.value == '') {
-        return;
-    } else {
-        todoList.setAttribute("style", "display:block");
-        let content = txt.value.trim();
-        obj.content = content;
-        obj.status = false;
-        datas.push(obj);
-        txt.value = '';
-        renderList();
-    }
+	if (txt.value == '') {
+		return;
+	}
+
+	addList();
+	renderList();
+	txt.value = '';
 })
+
+txt.addEventListener('keyup', function (e) {
+	if (e.keyCode === 13) {
+		if (txt.value == '') {
+			return;
+		}
+
+		addList();
+		renderList();
+		txt.value = '';
+	}
+
+	if (e.keyCode === 27) {
+		txt.value = '';
+	}
+})
+
+btnFilter.addEventListener("click", function (e) {
+	btnStatus(e.target);
+	dataFilter(e.target);
+})
+
+list.addEventListener('click', function (e) {
+	switch (e.target.nodeName) {
+		case 'IMG':
+			let num = e.target.getAttribute("data-num");
+			console.log(num);
+			datas.splice(num, 1);
+			break;
+		case 'INPUT':
+			let i = e.target.getAttribute('id');
+			datas[i].status = e.target.checked;
+			break;
+		default:
+			return;
+	}
+	renderList();
+})
+
+clear.addEventListener("click", function (e) {
+	cleanItem();
+	renderList();
+})
+
 //渲染畫面
 function renderList() {
-    todoCount();
-    const btnFocus = document.querySelector('.focus');
-    btnStatus(btnFocus);
-    dataFilter(btnFocus);
+	localStorage.setItem('datas', JSON.stringify(datas));//array to Json
+	todoCount();
+	const btnFocus = document.querySelector('.focus');
+	btnStatus(btnFocus);
+	dataFilter(btnFocus);
 }
 
 //計算待辦數
 function todoCount() {
-    let count = 0;
-    datas.forEach(function (item, index) {
-        if (!item.status)
-            count++;
-    })
-    tot.textContent = `${count}個待完成項目`;
+	let count = 0;
+	datas.forEach(function (item, index) {
+		if (!item.status)
+			count++;
+	})
+	tot.textContent = `${count}個待完成項目`;
 }
+
+//新增事項
+function addList() {
+	let obj = { status: false };
+	todoList.setAttribute("style", "display:block");
+	let content = txt.value.trim();
+	obj.content = content;
+	datas.push(obj);
+}
+
 //取得選取的按鈕
 function btnStatus(btnFocus) {
-    if (btnFocus.value == '全部') {
-        btnTodo.classList.remove('focus');
-        btnComplete.classList.remove('focus');
-    } else if (btnFocus.value == '待完成') {
-        btnAll.classList.remove('focus');
-        btnComplete.classList.remove('focus');
-    } else {
-        btnAll.classList.remove('focus');
-        btnTodo.classList.remove('focus');
-    }
+	btnAll.classList.remove('focus');
+	btnTodo.classList.remove('focus');
+	btnComplete.classList.remove('focus');
+	//新增CLASS
+	btnFocus.classList.add('focus')
 }
+
 //資料篩選
 function dataFilter(btnFocus) {
-    let str = '';
-    if (btnFocus.value == '全部') {
-        datas.forEach(function (item, index) {
-            if (item.status) {
-                str += `<li><input type="checkbox" id="${index}" checked><label for="${index}"><span></span>${item.content}</label><img src="https://hexschool.github.io/js-todo/assets/cancel.jpg" class="del" data-num="${index}"></li>`;
-            } else {
-                str += `<li><input type="checkbox" id="${index}"><label for="${index}"><span></span>${item.content}</label><img src="https://hexschool.github.io/js-todo/assets/cancel.jpg" class="del" data-num="${index}"></li>`;
-            }
-        })
-    }
-    if (btnFocus.value == '待完成') {
-        datas.forEach(function (item, index) {
-            if (!item.status) {
-                str += `<li><input type="checkbox" id="${index}"><label for="${index}"><span></span>${item.content}</label><img src="https://hexschool.github.io/js-todo/assets/cancel.jpg" class="del" data-num="${index}"></li>`;
-            }
-        })
-    } else if (btnFocus.value == '已完成') {
-        datas.forEach(function (item, index) {
-            if (item.status) {
-                str += `<li><input type="checkbox" id="${index}" checked><label for="${index}"><span></span>${item.content}</label><img src="https://hexschool.github.io/js-todo/assets/cancel.jpg" class="del" data-num="${index}"></li>`;
-            }
-        })
-    }
-    list.innerHTML = str;
+	let str = '';
+	if (btnFocus.value == '全部') {
+		datas.forEach(function (item, index) {
+			let checked = item.status ? 'checked' : '';
+			str += `<li><input type="checkbox" id="${index}" ${checked}><label for="${index}"><span></span>${item.content}</label><img src="https://hexschool.github.io/js-todo/assets/cancel.jpg" class="del" data-num="${index}"></li>`;
+		})
+	}
+
+	if (btnFocus.value == '待完成') {
+		datas.forEach(function (item, index) {
+			if (!item.status) {
+				str += `<li><input type="checkbox" id="${index}"><label for="${index}"><span></span>${item.content}</label><img src="https://hexschool.github.io/js-todo/assets/cancel.jpg" class="del" data-num="${index}"></li>`;
+			}
+		})
+	}
+
+	if (btnFocus.value == '已完成') {
+		datas.forEach(function (item, index) {
+			if (item.status) {
+				str += `<li><input type="checkbox" id="${index}" checked><label for="${index}"><span></span>${item.content}</label><img src="https://hexschool.github.io/js-todo/assets/cancel.jpg" class="del" data-num="${index}"></li>`;
+			}
+		})
+	}
+	list.innerHTML = str;
 }
 
-btnFilter.addEventListener("click", function (e) {
-    e.target.classList.add('focus');
-    btnStatus(e.target);
-    const btnFocus = document.querySelector('.focus')
-    dataFilter(btnFocus);
-})
-
-list.addEventListener("click", function (e) {
-    if (e.target.nodeName == 'IMG') {
-        let num = e.target.getAttribute("data-num");
-        datas.splice(num, 1);
-    } else if (e.target.nodeName != 'INPUT') {
-        return;
-    } else {
-        let i = e.target.getAttribute('id');
-        if (e.target.checked) {
-            datas[i].status = true;
-        } else {
-            datas[i].status = false;
-        }
-    }
-    console.log(datas);
-    renderList();
-})
-clear.addEventListener("click", function (e) {
-    cleanItem();
-    renderList();
-})
-
-
+//清除已完成項目
 function cleanItem() {
-    datas.forEach(function (item, index) {
-        if (item.status) {
-            datas.splice(index, 1);
-        }
-    })
+	for (let i = datas.length - 1; i >= 0; i--) {
+		if (datas[i].status) {
+			datas.splice(i, 1);
+		}
+	}
 }
